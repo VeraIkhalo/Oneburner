077a74789fba4d9e2f0d8e35f66027d47cde5fbf
Vera Ikhalo <ikhalo.oseaghe@gmail.com>
2021-03-31 23:59:42 +0100

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-const burger =document.getElementById("burger");
- const mobileNav=document.getElementById("mobile-nav");
-
- function toggle(){
-	 if(mobileNav.style.display === "none" ){
-		 mobileNav.style.display = "block";
-	 }
-	 else{mobileNav.style.display = "none";}
- }
- 
- burger.addEventListener("click", toggle) 
+// Hamburger Toggler
